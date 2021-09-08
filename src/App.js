6d50cfa47dbe43bbbf771f77179bfca01d2ca752
GiulianaSantilli24 <81430466+GiulianaSantilli24@users.noreply.github.com>
2021-09-08 13:10:37 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Botones from "./components/botones";
+
+function App() {
+  return (
+    <div className="layout">
+      <Botones/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/botones.js
@@ -0,0 +1,104 @@
+import react from "react";
+import Data from "./data.json"
+
+//inicializar el contador por fuera de la clase
+
+let contador = 1;
+
+//creo la clase Botones que extiende de react
+class Botones extends react.Component {
+
+    //le creo un constructor a Botones 
+    constructor(props) {
+        super(props)
+
+        this.state = { historiaNueva: Data[0].historia, ultimaEleccion: "", listaElecciones: [], opcionA: Data[0].opciones.a, opcionB: Data[0].opciones.b };
+
+        // agrego los handleChange al constructor
+
+        this.handleChangeReiniciarHistoria = this.handleChangeReiniciarHistoria.bind(this);
+
+        this.handleChangeOpcionA = this.handleChangeOpcionA.bind(this);
+
+        this.handleChangeOpcionB = this.handleChangeOpcionB.bind(this);
+    }
+
+
+    // creo la funcion handleChange para reiniciar la historia en cualquier momento y al finalizar la historia 
+
+    handleChangeReiniciarHistoria() {
+
+        contador = 1;
+
+        let historiaNueva = Data.filter(data => data.id === 1);
+
+        this.setState({ historiaNueva: Data[0].historia, ultimaEleccion: [], listaElecciones: [], opcionA: Data[0].opciones.a, opcionB: Data[0].opciones.b })
+    }
+
+    // creo la funcion handleChange para manejar la opcion A
+
+    handleChangeOpcionA() {
+        contador++;
+
+        let historiaNueva = Data.filter(data => data.id === contador + "a")[0];
+
+        let recordatorio = [...this.state.listaElecciones, "Opción A"];
+
+        if (contador < 6) {
+            this.setState({ ultimaEleccion: "Opción A", listaElecciones: recordatorio, historiaNueva: historiaNueva.historia, opcionA: historiaNueva.opciones.a, opcionB: historiaNueva.opciones.b });
+        }
+        else {
+            this.handleChangeReiniciarHistoria();
+            contador = 1;
+        }
+    }
+
+    // creo la funcion handleChange para manejar la opcion B
+
+    handleChangeOpcionB() {
+        contador++;
+
+        let historiaNueva = Data.filter(data => data.id === contador + "b")[0];
+
+        let recordatorio = [...this.state.listaElecciones, "Opción B"];
+
+        if (contador < 6) {
+            this.setState({ ultimaEleccion: "Opción B", listaElecciones: recordatorio, historiaNueva: historiaNueva.historia, opcionA: historiaNueva.opciones.a, opcionB: historiaNueva.opciones.b });
+        } else {
+            this.handleChangeReiniciarHistoria();
+            contador = 1;
+        }
+    }
+
+    //renderizo los botones, divs, botones y spans para que se vean correctamente en el html
+
+    render() {
+        return (
+            <>
+
+                <div className="historia">{this.state.historiaNueva}</div>
+                <div className="opcion">
+                    <button className="botones" onClick={this.handleChangeOpcionA}> Opción A</button>
+                    <span className="opciones">{this.state.opcionA}</span>
+                </div>
+                <br />
+                <div className="opcion">
+                    <button className="botones" onClick={this.handleChangeOpcionB}> Opción B</button>
+                    <span className="opciones">{this.state.opcionB}</span>
+                </div>
+                <div className="recordatorio">
+                    <p>última elección: {this.state.ultimaEleccion}</p>
+                    <p>recordatorio de todas las selecciones anteriores:<ul> {this.state.listaElecciones.map((eleccion, index) => <li key={eleccion + index}>{eleccion}</li>)}</ul></p>
+                    <div>
+                        <button className="botones" onClick={this.handleChangeReiniciarHistoria}> Reiniciar Historia </button>
+                    </div>
+                </div>
+
+            </>
+        )
+    }
+}
+
+//exporto la clase botones para utilizarla en App.js
+
+export default Botones;
